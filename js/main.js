22be25baa78691a62e615fd22e54994dee61842b
redcat <redcat-3@yandex.ru>
2022-11-21 18:42:32 +0800

    fj;f

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import {createPhotoDescription} from './util.js';
+import {createPictureElement} from './picture.js';
+
+const pictureElement = document.querySelector('.pictures');
+const picture = createPhotoDescription(2);
+pictureElement.append(createPictureElement(picture));
