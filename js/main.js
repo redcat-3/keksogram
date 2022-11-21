2c81caea52332135d1313d87c13900232bfb1009
redcat <redcat-3@yandex.ru>
2022-11-21 18:37:07 +0800

    dgfh

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import {createPhotoDescription} from './util.js';
-import {createPictureElement} from './picture.js';
-
-const pictureElement = document.querySelector('.pictures');
-const picture = createPhotoDescription(2);
-pictureElement.append(createPictureElement(picture));
-
